d12ac70faf038fd8f2c817e2eebeaf07d01ab2a0
VineethK <vineethk96@users.noreply.github.com>
2025-02-26 10:18:48 +0000

    chore: cleaning up my repo

--- traveler/android/app/.cxx/Debug/2g3o3o3t/arm64-v8a/CMakeFiles/3.22.1-g37088a8/CMakeSystem.cmake
+++ traveler/android/app/.cxx/Debug/2g3o3o3t/arm64-v8a/CMakeFiles/3.22.1-g37088a8/CMakeSystem.cmake
@@ -1,6 +1,6 @@
-set(CMAKE_HOST_SYSTEM "Linux-6.8.0-51-generic")
+set(CMAKE_HOST_SYSTEM "Linux-6.8.0-52-generic")
 set(CMAKE_HOST_SYSTEM_NAME "Linux")
-set(CMAKE_HOST_SYSTEM_VERSION "6.8.0-51-generic")
+set(CMAKE_HOST_SYSTEM_VERSION "6.8.0-52-generic")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
 include("/home/vineeth/Android/Sdk/ndk/26.3.11579264/build/cmake/android.toolchain.cmake")
